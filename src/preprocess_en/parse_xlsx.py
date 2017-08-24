eac2ae9db8d8f7b80d2495b5714220ff045c3329
ctinray <b03902072@ntu.edu.tw>
2017-08-24 17:12:43 +0800

    modify script to parse whole xlsx

--- src/preprocess_en/parse_xlsx.py
+++ src/preprocess_en/parse_xlsx.py
@@ -10,7 +10,7 @@ import sys
 import traceback
 
 
-def parse_ai2(df):
+def parse_xlsx(df):
     operands = []
     for formula in df['formula']:
         ops = list(filter(lambda x: x in '+-*', formula))
@@ -24,22 +24,15 @@ def parse_ai2(df):
     for question in df['question']:
         # split body and question
         punctuate_matches = list(re.finditer('[,.]', question))
-        last_punctuate_index = punctuate_matches[-1].span()[0]
-        questions.append(question[:last_punctuate_index + 1])
-        bodies.append(question[last_punctuate_index + 1:])
+        if len(punctuate_matches) > 0:
+            last_punctuate_index = punctuate_matches[-1].span()[0]
+        else:
+            last_punctuate_index = -1
+        bodies.append(question[:last_punctuate_index + 1])
+        questions.append(question[last_punctuate_index + 1:])
     df.drop('question', axis=1, inplace=True)
     df['Body'] = bodies
     df['Question'] = questions
-
-
-def parse_ilds(input, output):
-    df = pd.read_excel(input, header=0, sheetname='ilds')
-    pass
-
-
-def cmds(input, output):
-    df = pd.read_excel(input, header=0, sheetname='cmds')
-    pass
 
 
 def main():
@@ -50,10 +43,11 @@ def main():
     parser.add_argument('output', type=str, help='Output csv directory')
     args = parser.parse_args()
 
-    df_ai2 = pd.read_excel(args.input, sheetname='ai2',
+    for sheet in ['ai2', 'ilds', 'cmds']:
+        df = pd.read_excel(args.input, sheetname=sheet,
                            header=0, index_col=0)
-    parse_ai2(df_ai2)
-    df_ai2.to_csv(os.path.join(args.output, 'ai2.csv'))
+        parse_xlsx(df)
+        df.to_csv(os.path.join(args.output, '%s.csv' % sheet))
 
 
 if __name__ == '__main__':
