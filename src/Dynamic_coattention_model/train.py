01728baa732a1251b482d79327e1f5086c29e035
jim <jimlin7777@hotmail.com>
2017-08-31 09:52:38 +0800

    Dynamic coattention model folder

---
+++ src/Dynamic_coattention_model/train.py
@@ -0,0 +1,85 @@
+import argparse
+import numpy as np
+from nn import NNClassifier
+from utils import Preprocessor, interactive
+import pdb
+import sys
+import traceback
+import pandas as pd
+from IPython import embed
+
+
+def main():
+    parser = argparse.ArgumentParser(description='IIS project')
+    parser.add_argument('train', type=str, help='train.csv')
+    parser.add_argument('valid', type=str, help='valid.csv')
+    parser.add_argument('embedding', type=str, help='embedding.pickle')
+    parser.add_argument('--preprocess_args', type=str,
+                        default='preprocess_args.pickle',
+                        help='pickle to store preprocess arguments')
+    parser.add_argument('--valid_ratio', type=float,
+                        help='ratio of validation data.', default=0.1)
+    parser.add_argument('--n_iters', type=int, default=100,
+                        help='number of epochs')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='learning rate')
+    parser.add_argument('--batch_size', type=int, default=256,
+                        help='batch size')
+    parser.add_argument('--name', type=str, default='dnn',
+                        help='name for the model, will be the directory name for summary')
+    args = parser.parse_args()
+
+    preprocessor = Preprocessor(args.embedding)
+    train = preprocessor.load_data(args.train)
+    valid = preprocessor.load_data(args.valid)
+
+    clf = NNClassifier(valid=valid,
+                       learning_rate=args.lr,
+                       n_iters=args.n_iters,
+                       name=args.name,
+                       batch_size=args.batch_size,
+                       embedding=preprocessor.embedding, early_stop=20)
+    clf.fit(train['x'], train['y'])
+
+    valid['y_'], valid['y_prob'] = clf.predict(valid['x'], True)
+    train['y_'], train['y_prob'] = clf.predict(train['x'], True)
+
+    operators = ['+','-','*','/', '%']    
+
+    for i in range(len(operators)):
+        indices = np.where(valid['y'][:, i] == 1)
+        count = np.sum(valid['y_'][indices], axis=0)
+        print('operator %s: %d' % (operators[i], len(indices[0])))
+        for j in range(len(operators)):
+            print('    %s: %d' % (operators[j], count[j]))
+
+    data = pd.read_csv(args.valid)
+    data["Predict"] = list(valid['y_'])
+    data["Predict"] = data["Predict"].map(lambda x:operators[list(x).index(1)])
+    #print(data.shape)
+    data["Prob"] = list(valid["y_prob"])
+    data = data[data["Predict"] != data["Operand"]]
+    #print(data.shape)
+    data.to_csv("incorrect.csv")
+
+    data = pd.read_csv(args.train)
+    
+    data["Predict"] = list(train['y_'])
+    data["Predict"] = data["Predict"].map(lambda x:operators[list(x).index(1)])
+    #print(data.shape)
+    data["Prob"] = list(train["y_prob"])
+    data = data[data["Operand"]!="s"]
+    data = data[data["Predict"] != data["Operand"]]
+    #print(data.shape)
+    data.to_csv("incorrect_train.csv")
+    # interactive interface
+    while True:
+        interactive(clf, preprocessor, operators)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        type, value, tb = sys.exc_info()
+        traceback.print_exc()
+        pdb.post_mortem(tb)
---
+++ src/Dynamic_coattention_model/utils.py
@@ -0,0 +1,133 @@
+import pickle
+import numpy as np
+import pandas as pd
+import pdb
+import os
+
+class Preprocessor:
+
+    @staticmethod
+    def encode_text(text, word_dict):
+        encoded = []
+        for row in text:
+            # Word-based
+            words = row.strip().split()
+            # Char-based
+            #words = [c for c in row if u'\u4e00'<= c <= u'\u9fff']
+            word_indices = []
+            for word in words:
+                if word in word_dict:
+                    word_indices.append(word_dict[word])
+                else:
+                    # [TODO] Cope with OOV
+                    pass
+
+            encoded.append(word_indices)
+
+        return encoded
+
+    @staticmethod
+    def pad_text(text, max_len=None):
+        if max_len is None:
+            max_len = max(map(len, text))
+    
+        padded = np.zeros((len(text), max_len))
+
+        for i in range(len(text)):
+            to_copy = min(len(text[i]), max_len)
+            padded[i, :to_copy] = text[i][:to_copy]
+
+        return padded
+
+    @staticmethod
+    def split_valid(data, valid_ratio, shuffle=True):
+        indices = np.arange(data['x'].shape[0])
+        if shuffle:
+            np.random.shuffle(indices)
+            data['x'] = data['x'][indices]
+            data['y'] = data['y'][indices]
+
+        n_valid = int(data['x'].shape[0] * valid_ratio)
+        train = {'x': data['x'][n_valid:], 'y': data['y'][n_valid:]}
+        valid = {'x': data['x'][:n_valid], 'y': data['y'][:n_valid]}
+
+        return train, valid, indices
+
+    @staticmethod
+    def encode_labels(labels):
+        operators = ['+', '-', '*', '/', '%']
+        indices = [operators.index(label) for label in labels]
+        encoded = np.zeros((len(labels), len(operators)))
+        encoded[np.arange(len(labels)), indices] = 1
+        return encoded
+
+
+    def __init__(self, embedding_filename):
+
+        # load embedding and make embedding matrix
+        with open(embedding_filename, 'rb') as f:
+            obj = pickle.load(f)
+            self.word_dict = obj['word_dict']
+            self.embedding = obj['embedding']
+
+        self._max_len = None
+
+    def load_data(self, filename):
+        # read data
+        df = pd.read_csv(filename).fillna(value='')
+        
+        # preprocess text
+        data = {}
+        data['Body'] = self.encode_text(df['Body'],
+                                        self.word_dict)
+        data['Question'] = self.encode_text(df['Question'],
+                                            self.word_dict)
+
+        if self._max_len is None:
+            max_len_body = max(map(len, data['Body']))
+            max_len_question = max(map(len, data['Question']))
+            self._max_len = max(max_len_body, max_len_question)
+
+        data['Body'] = self.pad_text(data['Body'], self._max_len)
+        data['Question'] = self.pad_text(data['Question'], self._max_len)
+        # stack Body and Question as x
+        data['x'] = np.zeros((data['Body'].shape[0],
+                              2,
+                              data['Body'].shape[1]))
+        data['x'][:, 0, :] = data['Body']
+        data['x'][:, 1, :data['Question'].shape[1]] = data['Question']
+
+        # preprocess tags to one hot
+        data['y'] = self.encode_labels(df['Operand'])
+
+
+
+        return data
+
+
+def BatchGenerator(X, y, batch_size, shuffle=True):
+    if shuffle:
+        indices = np.random.permutation(X.shape[0])
+        X = X[indices]
+        y = y[indices]
+
+    for i in range(X.shape[0] // batch_size + 1):
+        batch = {'x': X[i * batch_size: (i + 1) * batch_size],
+                 'y': y[i * batch_size: (i + 1) * batch_size]}
+        yield batch
+
+
+def interactive(clf, preprocessor, operators):
+    test = {}
+    test['Body'] = [input("Body: ")]
+    test['Question'] = [input("Question: ")]
+    test['Operand'] = [input("Operand: ")]
+    # dump to temp file
+    tempdf = pd.DataFrame(data=test)
+    tempdf.to_csv("temp.csv")
+    # load and preprocess
+    test = preprocessor.load_data("temp.csv")
+    test['y_'], test['y_prob'] = clf.predict(test['x'], True)
+    test['y_prob'] = test['y_prob'].reshape((-1))
+    for i, op in enumerate(operators):
+        print("Op:{}, predict prob: {}".format(op, test['y_prob'][i]))
